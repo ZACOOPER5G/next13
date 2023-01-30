45509e90639dcb93fc7f01c092545205b17cefab
Zac Cooper <zacooper5g@outlook.com>
2023-01-31 10:37:25 +1100

    built basic search component

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client"
+
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 const Search = () => {
-  return (
-    <div>I am a search component</div>
-  )
-}
+	const [search, setSearch] = useState("");
+	const router = useRouter();
 
-export default Search
+	const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setSearch("");
+		router.push(`/search/${search}`);
+	};
+
+	return (
+		<form onSubmit={handleSearch}>
+			<input
+				type="text"
+				value={search}
+				placeholder="Enter the search term"
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<button type="submit" className="btn bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">
+				Search
+			</button>
+		</form>
+	);
+};
+
+export default Search;
